428b1d2a520527f672719c884524288888cba5bd
geonwoo.jeong <doscmDev@gmail.com>
2020-03-24 18:12:36 +0900

    refactor: add scroll animation

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeViewer from './HomeViewer'
 
 const HomeContainer = () => {
-  return <HomeViewer />
+  const [screenHeight, setScreenHeight] = useState<boolean>(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const handleScroll = (e: Event) => {
+    if (window.scrollY > 0) {
+      setScreenHeight(true)
+    } else {
+      setScreenHeight(false)
+    }
+  }
+
+  return <HomeViewer screenHeight={screenHeight} />
 }
 
 export default HomeContainer
--- src/Routes/Home/HomeViewer.tsx
+++ src/Routes/Home/HomeViewer.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Header, Introduce } from 'src/Components/Molecules'
 
-const HomeViewer = () => {
-  return <div>HomeViewer</div>
+const Container = styled.div``
+
+interface IHome {
+  screenHeight: boolean
+}
+
+const HomeViewer = ({ screenHeight }: IHome) => {
+  return (
+    <Container>
+      {/* header */}
+      <Header screenHeight={screenHeight} />
+
+      {/* banner */}
+      <Introduce />
+    </Container>
+  )
 }
 
 export default HomeViewer
