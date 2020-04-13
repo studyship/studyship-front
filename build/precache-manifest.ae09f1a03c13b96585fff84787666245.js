208c2b402aae00d5a3e1b519f200d03a9e35ac07
geonwoo.jeong <doscmDev@gmail.com>
2020-04-13 16:20:33 +0900

    fix: deploy build file

---
+++ build/precache-manifest.ae09f1a03c13b96585fff84787666245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8bd4bf073aa3e05f97b1a0414120ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d28c9f1bc7efb10309c0",
+    "url": "/static/js/2.e540bb2f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e540bb2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3936eff33f49f4bea4d",
+    "url": "/static/js/main.6af850d8.chunk.js"
+  },
+  {
+    "revision": "d1ffc290f534e62a938d",
+    "url": "/static/js/runtime-main.d1113993.js"
+  }
+]);
