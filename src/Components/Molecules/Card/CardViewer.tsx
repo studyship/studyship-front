4013f4f2e594fa3e96453ce9ad4eefb7a0f9f8f7
geonwoo.jeong <doscmDev@gmail.com>
2020-04-20 20:15:04 +0900

    style: change card view

--- src/Components/Molecules/Card/CardViewer.tsx
+++ src/Components/Molecules/Card/CardViewer.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Title } from 'src/Components/Atoms'
+import {
+  Title,
+  Icon,
+  BoldText,
+  NormarText,
+  HorizontalBar,
+} from 'src/Components/Atoms'
 import { ITitle } from 'src/Components/Atoms/Title/types'
 import { descriptionColor } from 'src/styles/Colors'
+import BockmarkIcon from 'src/styles/icons/icon_bookmark_fill.svg'
+import ReactLogo from 'src/styles/images/react-logo.png'
 
 const Container = styled.div`
-  flex: 0 0 auto;
-  margin-bottom: 30px;
-  padding: 27px 22px;
-  width: 355px;
-  height: 192px;
+  display: flex;
+  flex-direction: column;
+  width: 360px;
+  height: 360px;
   border: 0.5px solid #e3e3e0;
   box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.08);
   border-radius: 10px;
-  margin: 0px 20px;
-  :first-child,
-  :last-child {
-    margin-left: 0px;
-  }
-
+  margin: 1rem;
   @media screen and (max-width: 450px) {
     flex: 0 0 90%;
     display: flex;
@@ -31,18 +33,44 @@ const Container = styled.div`
 const STitle = styled(Title)`
   margin-bottom: 27px;
 `
-const DescriptionBox = styled.div`
+const CategoryBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`
+const Category = styled.div`
+  color: ${descriptionColor};
+  font-size: 14px;
+  margin-bottom: 18px;
+  :nth-child(2n) {
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+`
+const TitleBox = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+`
+const IconBox = styled.div`
+  width: 18px;
+  height: 25px;
+`
+const CardSection = styled.div`
   width: 100%;
+  height: 180px;
+  :nth-child(3) {
+    padding: 11px 18px;
+  }
 `
-const CardDescription = styled.div`
-  color: ${descriptionColor};
-  font-size: 14px;
-  margin: 14px 0px;
+const Target = styled.div`
+  display: flex;
+  flex-direction: row;
+  > *:nth-child(1) {
+    margin-right: 5px;
+  }
 `
-
 interface ICard {
   titleStyles: ITitle
 }
@@ -50,17 +78,27 @@ interface ICard {
 const CardViewer = ({ titleStyles }: ICard) => {
   return (
     <Container>
-      <Title
-        type={titleStyles.type}
-        text={titleStyles.text}
-        size={titleStyles.size}
-      />
-      <DescriptionBox>
-        <CardDescription>qwe</CardDescription>
-        <CardDescription>asd</CardDescription>
-        <CardDescription>zxc</CardDescription>
-        <CardDescription>123</CardDescription>
-      </DescriptionBox>
+      <CardSection>
+        <Icon imgSrc={ReactLogo} />
+      </CardSection>
+      <HorizontalBar />
+      <CardSection>
+        <TitleBox>
+          <BoldText fontSize="18px" text="스터디 제목입니다" />
+          <IconBox>
+            <img src={BockmarkIcon} />
+          </IconBox>
+        </TitleBox>
+        <CategoryBox>
+          <Category>qwe</Category>
+          <Category>></Category>
+          <Category>123</Category>
+        </CategoryBox>
+        <Target>
+          <BoldText fontSize="14px" text="목표" />
+          <NormarText fontSize="14px" text="페이스북 클론 코딩" />
+        </Target>
+      </CardSection>
     </Container>
   )
 }
