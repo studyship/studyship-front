b69e501eea9b5ba45e4f2069d2f0c0403c5fcb8e
geonwoo.jeong <doscmDev@gmail.com>
2020-04-24 15:09:03 +0900

    refactor: move file location

--- src/lib/createAsyncThunk.ts
+++ src/lib/createAsyncThunk.ts
@@ -1,5 +1,6 @@
 import { AsyncActionCreatorBuilder } from 'typesafe-actions'
 import { Dispatch } from 'redux'
+import { startLoading, finishLoading } from 'src/store/modules/loading/actions'
 
 type AnyAsyncActionCreator = AsyncActionCreatorBuilder<any, any, any>
 
@@ -15,9 +16,11 @@ const createAsyncThunk = <
     return async (dispatch: Dispatch) => {
       const { request, success, failure } = asyncActionCreator
       dispatch(request())
+      dispatch(startLoading(request().type))
       try {
         const res = await promiseCreator(...params)
         dispatch(success(res))
+        dispatch(finishLoading(request().type))
       } catch {
         dispatch(failure())
       }
