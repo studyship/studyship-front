1e2d3ac6e6e716ec3b69f300bad6179e448bb3ad
geonwoo.jeong <doscmDev@gmail.com>
2020-04-24 15:10:27 +0900

    feat: add redux store

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import App from './Components/App'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/modules'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
 
 serviceWorker.unregister()
