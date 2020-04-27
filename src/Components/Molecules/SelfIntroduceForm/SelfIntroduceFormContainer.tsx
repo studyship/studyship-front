3d7722091b621bb833097867086303648d58c470
geonwoo.jeong <doscmDev@gmail.com>
2020-04-27 23:40:55 +0900

    refactor: rename custom hooks

--- src/Components/Molecules/SelfIntroduceForm/SelfIntroduceFormContainer.tsx
+++ src/Components/Molecules/SelfIntroduceForm/SelfIntroduceFormContainer.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import SelfIntroduceFormViewer from './SelfIntroduceFormViewer'
 import { ISelfIntroduceForm } from './types'
-import { useInputHandler } from 'src/hooks'
+import { useInputText } from 'src/hooks'
 import { inputValidators } from 'src/lib'
 
 const SelfIntroduceFormContainer = ({
   handleNextStage,
 }: ISelfIntroduceForm) => {
   const { inputLengthLimits } = inputValidators
-  const { bind } = useInputHandler('', inputLengthLimits(50))
+  const { bind } = useInputText('', inputLengthLimits(50))
   return (
     <SelfIntroduceFormViewer handleNextStage={handleNextStage} bind={bind} />
   )
