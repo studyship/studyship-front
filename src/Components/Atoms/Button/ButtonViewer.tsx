d7a75fa0b88a16394566721ccfb7a73e9e2e3464
geonwoo.jeong <doscmDev@gmail.com>
2020-04-27 23:40:40 +0900

    refactor: update button

--- src/Components/Atoms/Button/ButtonViewer.tsx
+++ src/Components/Atoms/Button/ButtonViewer.tsx
@@ -4,26 +4,49 @@ import styled from 'styled-components'
 const Button = styled.button`
   padding: 14px 0px;
   width: 100%;
+  white-space: nowrap;
   text-align: center;
   vertical-align: center;
   border-radius: 5px;
-  font-size: 16px;
   font-weight: normal;
-  color: white;
+  font-size: ${(props: IButtonStyle) =>
+    props.fontSize ? props.fontSize : '15px'};
+  color: ${(props: IButtonStyle) =>
+    props.fontColor ? props.fontColor : '#ffffff'};
+  background-color: ${(props: IButtonStyle) =>
+    props.bgColor ? props.bgColor : props.bgColor};
   border: none;
   outline: none;
   cursor: pointer;
 `
 
-type TButton = {
+interface IButtonStyle {
+  fontSize?: string
+  fontColor?: string
+  bgColor?: string
+}
+interface IButton extends IButtonStyle {
   className?: string
   text: string
   onClick?: () => void
 }
 
-const ButtonViewer = ({ className, text, onClick }: TButton) => {
+const ButtonViewer = ({
+  className,
+  text,
+  onClick,
+  fontSize,
+  fontColor,
+  bgColor,
+}: IButton) => {
   return (
-    <Button className={className} onClick={onClick}>
+    <Button
+      className={className}
+      onClick={onClick}
+      fontSize={fontSize}
+      fontColor={fontColor}
+      bgColor={bgColor}
+    >
       {text}
     </Button>
   )
