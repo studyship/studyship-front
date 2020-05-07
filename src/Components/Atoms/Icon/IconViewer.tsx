70abcb42d30ff598d73da3f9bc4911b4c02e8aef
geonwoo.jeong <doscmDev@gmail.com>
2020-05-07 12:08:42 +0900

    refactor: extension delimited behavior

--- src/Components/Atoms/Icon/IconViewer.tsx
+++ src/Components/Atoms/Icon/IconViewer.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import LazyLoad from 'react-lazyload'
 import styled from 'styled-components'
 import { IIconViewer } from './types'
+import { getFileExtentions } from 'src/lib'
 
 const SLazyLoad = styled(LazyLoad)``
-const Image = styled.img`
+const Png = styled.img`
   width: 100%;
   height: 100%;
+  border-radius: inherit;
+`
+const Svg = styled.img`
   border-radius: inherit;
 `
 
@@ -14,7 +18,11 @@ const IconViewer = ({ imgSrc, onClick }: IIconViewer) => {
   // 레이지로딩 시 이미지가 새로 렌더링이 되지 않는 문재 해결 해야함
   return (
     <SLazyLoad>
-      <Image src={imgSrc} />
+      {getFileExtentions(imgSrc) === 'svg' ? (
+        <Svg src={imgSrc} />
+      ) : (
+        <Png src={imgSrc} />
+      )}
     </SLazyLoad>
   )
 }
