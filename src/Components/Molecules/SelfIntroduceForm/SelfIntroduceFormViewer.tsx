cd0fc17bf4043997d09c4e6bb3f57b784035e847
geonwoo.jeong <doscmDev@gmail.com>
2020-05-12 14:51:13 +0900

    style: change textarea

--- src/Components/Molecules/SelfIntroduceForm/SelfIntroduceFormViewer.tsx
+++ src/Components/Molecules/SelfIntroduceForm/SelfIntroduceFormViewer.tsx
@@ -7,15 +7,17 @@ import { mainColorBlue } from 'src/styles/Colors'
 
 const Container = styled.div``
 const STextareaAutosize = styled(TextareaAutosize)`
+  padding: 7px 9px;
+  min-height: 160px;
   margin-bottom: 6px;
   width: 100%;
   font-weight: normal;
-  font-size: 18px;
+  font-size: 16px;
   background-color: transparent;
-  border: none;
-  border-bottom: 2px solid ${mainColorBlue};
+  border: 1px solid #e6e6e6;
   resize: none;
   outline: none;
+  border-radius: 5px;
   ::placeholder {
     color: #b3b3b3;
   }
@@ -37,7 +39,7 @@ const SelfIntroduceFormViewer = ({
   return (
     <Container>
       <STextareaAutosize
-        placeholder="ex) 개발하는 디자이너가 되고 싶은 김수민입니다."
+        placeholder="예) 디자인과 개발 모두 평정하고싶은 꿈나무 디발자입니다. 자유로운 분위기 속에서 공부하는 것을 지향하지만 루즈해지는 분위기는 좋아하지 않습니다."
         {...bind}
       />
       <TextLimits fontSize="18px" text={`${bind.value.length}/50`} />
