b1c9ccfa38771c3c9ffea1162c53938477cfc576
geonwoo.jeong <doscmDev@gmail.com>
2020-07-07 02:30:56 +0900

    feat: add hooks for NavTab

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,11 @@
 import useInputText from './useInputText'
 import useInputCheckbox from './useInputCheckbox'
 import useComponentVisible from './useComponentVisible'
+import useHandleSelectTab from './useHandleSelectTab'
 
-export { useInputText, useInputCheckbox, useComponentVisible }
+export {
+  useInputText,
+  useInputCheckbox,
+  useComponentVisible,
+  useHandleSelectTab,
+}
