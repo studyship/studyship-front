88c4ceef0ad98df992de49cf25753c079c91eb79
geonwoo.jeong <doscmDev@gmail.com>
2020-09-26 15:56:56 +0900

    fix: change mixin for marginStyles

--- src/styles/mixin/index.ts
+++ src/styles/mixin/index.ts
@@ -3,21 +3,17 @@ import { css } from 'styled-components'
 import { MarginStyleProps, PaddingStyleProps } from 'src/@types'
 
 export const marginStyles = css<MarginStyleProps>`
-  ${({ margin, marginTop, marginBottom, marginLeft, marginRight }) =>
-    margin
-      ? `margin: ${margin}`
-      : `margin-top: ${marginTop && marginTop};
-  margin-bottom: ${marginBottom && marginBottom};
-  margin-left: ${marginLeft && marginLeft};
-  margin-right: ${marginRight && marginRight};`};
+  margin-top: ${({ marginTop }) => marginTop && marginTop};
+  margin-bottom: ${({ marginBottom }) => marginBottom && marginBottom};
+  margin-left: ${({ marginLeft }) => marginLeft && marginLeft};
+  margin-right: ${({ marginRight }) => marginRight && marginRight};
+  margin: ${({ margin }) => margin && margin};
 `
 
 export const paddingStyles = css<PaddingStyleProps>`
-  ${({ padding, paddingTop, paddingBottom, paddingLeft, paddingRight }) =>
-    padding
-      ? `padding: ${padding}`
-      : `padding-top: ${paddingTop && paddingTop};
-padding-bottom: ${paddingBottom && paddingBottom};
-padding-left: ${paddingLeft && paddingLeft};
-padding-right: ${paddingRight && paddingRight};`};
+  padding-top: ${({ paddingTop }) => paddingTop && paddingTop};
+  padding-bottom: ${({ paddingBottom }) => paddingBottom && paddingBottom};
+  padding-left: ${({ paddingLeft }) => paddingLeft && paddingLeft};
+  padding-right: ${({ paddingRight }) => paddingRight && paddingRight};
+  padding: ${({ padding }) => padding && padding};
 `
