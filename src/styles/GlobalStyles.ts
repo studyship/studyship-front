61760e56f5d5f5dd3c298dabd677eda50a1d2b1b
geonwoo.jeong <doscmDev@gmail.com>
2020-10-18 17:25:56 +0900

    Update GlobalStyles.ts

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -19,19 +19,12 @@ const globalStyle = createGlobalStyle`
         background-color: ${backgroundColor};
         color:${mainColorBlack};
     }
-    @font-face { font-family: 'DM Sans'; src: url('./fonts/DMSans-Bold.ttf') format('truetype'); font-weight: normal; font-style: normal; }
-    @font-face { font-family: 'DM Sans'; src: url('./fonts/DMSans-Regular.ttf') format('truetype'); font-weight: normal; font-style: normal; }
-    @font-face { font-family: 'DM Sans'; src: url('./fonts/DMSans-Medium.ttf') format('truetype'); font-weight: normal; font-style: normal; }
+    @font-face { font-family: 'DM Sans'; src: url('./fonts/DMSans-Bold.ttf') format('font-woff'); font-weight: normal; font-style: normal; unicode-range:U+0041-005A, U+0061-007A; }
+    @font-face { font-family: 'DM Sans'; src: url('./fonts/DMSans-Regular.ttf') format('font-woff'); font-weight: normal; font-style: normal; unicode-range:U+0041-005A, U+0061-007A; }
+    @font-face { font-family: 'DM Sans'; src: url('./fonts/DMSans-Medium.ttf') format('font-woff'); font-weight: normal; font-style: normal; unicode-range:U+0041-005A, U+0061-007A; }
     @font-face { font-family: 'Noto Sans KR'; src: url('./fonts/NotoSansKR-Bold.woff') format('font-woff'); font-weight: normal; font-style: normal; }
     @font-face { font-family: 'Noto Sans KR'; src: url('./fonts/NotoSansKR-Regular.woff') format('font-woff'); font-weight: normal; font-style: normal; }
     @font-face { font-family: 'Noto Sans KR'; src: url('./fonts/NotoSansKR-Thin.woff') format('font-woff'); font-weight: normal; font-style: normal; }
-
-    *:lang(en) { 
-    font-family: Noto Sans KR; 
-    } 
-    *:lang(ko) { 
-    font-family: DM Sans; 
-    } 
 `
 
 export default globalStyle
