801464dd2b901255666d3f266cb92ec731a6a71a
geonwoo.jeong <doscmDev@gmail.com>
2020-12-20 03:21:24 +0900

    refactor: handle heart icon

--- src/Components/Atoms/Heart.tsx
+++ src/Components/Atoms/Heart.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { ReactComponent as HeartIcon } from 'src/styles/icons/ss_empty_heart.svg'
-import fillHeart from 'src/styles/icons/ss_fill_heart.svg'
-
+import { ReactComponent as HeartIcon } from 'src/styles/icons/ss_heart.svg'
+import { Icon } from '.'
 interface HeartProps {
   isFill: boolean
   heartSize?: number
   onClick?: VoidFunction
 }
-
 const Heart = ({ isFill, onClick }: HeartProps) => {
-  return <HeartIcon onClick={onClick} cursor="pointer" fill="red" />
+  return (
+    <HeartIcon
+      onClick={onClick}
+      cursor="pointer"
+      {...(!isFill && {
+        fill: 'none',
+        stroke: '#b3b3b3',
+        strokeWidth: '3',
+      })}
+    />
+  )
 }
 
 export default Heart
